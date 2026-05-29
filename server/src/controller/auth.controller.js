e174c62ec121a813e35ebdb15cd9a392b0e22779
Himanshu <er.himanshuk.00@gmail.com>
2026-05-29 07:38:53 +0530

    food partner apis created

--- server/src/controller/auth.controller.js
+++ server/src/controller/auth.controller.js
@@ -1,7 +1,7 @@
 import User from "../model/user.model.js"
 import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken"
-
+import FoodPartner from "../model/foodPartner.model.js"
 
 async function register(req, res){
     const {name, email, password} = req.body
@@ -109,4 +109,118 @@ async function logout(req, res){
     })
 }
 
-export {register, login, logout}
+// food partner
+async function foodPartnerRegister(req, res){
+    const {name, email, password} = req.body
+
+    if(!name || !email || !password){
+        return res.status(400).json({
+            message: "please enter details"
+        })
+    }
+
+   try {
+        const isExist = await FoodPartner.findOne({email})
+
+        if(isExist){
+            return res.status(400).json({
+                message: "Food partner already exist"
+            })
+        }
+ 
+        const hashPass = await bcrypt.hash(password, 10)
+
+        const foodPartner = await FoodPartner.create({
+            name,
+            email,
+            password: hashPass
+        })
+
+        const token = jwt.sign(
+            {id: foodPartner._id},
+            process.env.JWT_SECRET
+        )
+
+        res.cookie("token", token)
+
+        res.status(201).json({
+            message: "user created successfully",
+            foodPartner:{
+                id: foodPartner._id,
+                name: foodPartner.name,
+                email: foodPartner.email
+            }
+        })
+    }catch (error) {
+        return res.status(400).json({
+            error: error.message
+        })
+   }
+
+}
+
+async function foodPartnerLogin(req, res){
+    const {email, password} = req.body
+
+    if(!email || !password){
+        return res.status(400).json({
+            message: "please enter details"
+        })
+    }
+
+   try {
+        const foodPartner = await FoodPartner.findOne({email})
+
+        if(!foodPartner){
+            return res.status(400).json({
+                message: "foodPartner does not exist"
+            })
+        }
+        
+        const passVerify = bcrypt.compare(password, foodPartner.password)
+
+        if(!passVerify){
+            return res.status(400).json(
+                {message: "password is incorrect"}
+            )
+        }
+
+        const token = jwt.sign(
+            {id: foodPartner._id},
+            process.env.JWT_SECRET
+        )
+
+        res.cookie("token", token)
+
+        res.status(201).json({
+            message: "logged in successfully",
+            foodPartner:{
+                id: foodPartner._id,
+                name: foodPartner.name,
+                email: foodPartner.email
+            }
+        })
+
+    }catch (error) {
+        return res.status(400).json({
+            error: error.message
+        })
+   }
+
+}
+
+async function foodPartnerLogout(req, res){
+    res.clearCookie("token")
+    res.status(200).json({
+        message: "foodPartner logged out successfully"
+    })
+}
+
+export {
+    register, 
+    login, 
+    logout,
+    foodPartnerRegister,
+    foodPartnerLogin,
+    foodPartnerLogout
+}
--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -1,10 +1,16 @@
 import express from "express"
-import { register, login, logout } from "../controller/auth.controller.js"
+import { register, login, logout, foodPartnerRegister, foodPartnerLogin, foodPartnerLogout } from "../controller/auth.controller.js"
 
 const authRouter = express.Router()
 
+// user routes
 authRouter.route("/register").post(register)
 authRouter.route("/login").post(login)
 authRouter.route("/logout").post(logout)
+
+// food partner routes
+authRouter.route("/food-partner/register").post(foodPartnerRegister)
+authRouter.route("/food-partner/login").post(foodPartnerLogin)
+authRouter.route("/food-partner/logout").post(foodPartnerLogout)
 
 export default authRouter
