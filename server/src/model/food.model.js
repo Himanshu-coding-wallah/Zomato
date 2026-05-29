6c94a2b6fc7d2931bdcefa3dab49cfdb24b0d96f
Himanshu <er.himanshuk.00@gmail.com>
2026-05-29 07:45:08 +0530

    food model created

---
+++ server/src/model/food.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const foodSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    video : {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+    },
+    foodPartner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "FoodPartner"
+    }
+})
+
+const Food = mongoose.model("Food", foodSchema)
+export default Food
